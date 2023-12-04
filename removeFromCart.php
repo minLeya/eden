584a29ea78e6f167ba977edb4853fa2fa7faba97
Leya <leya.min@mail.ru>
2023-12-04 12:24:57 +0300

    delete button works but not correctly

--- removeFromCart.php
+++ removeFromCart.php
@@ -13,43 +13,38 @@ if ($_SERVER['REQUEST_METHOD'] === 'POST' && isset($_POST['productId'])) {
         die("Connection failed: " . $conn->connect_error);
     }
 
-    // Получение текущего количества
+    // Извлечение текущего значения quantity
     $getQuantitySql = "SELECT quantity FROM cart WHERE id_product = ?";
     $stmtGetQuantity = $conn->prepare($getQuantitySql);
     $stmtGetQuantity->bind_param("i", $productId);
     $stmtGetQuantity->execute();
     $stmtGetQuantity->bind_result($quantity);
     $stmtGetQuantity->fetch();
+    $stmtGetQuantity->close();
 
+    // Обновление или удаление товара
     if ($quantity > 1) {
-        // Уменьшение количества на 1
         $updateSql = "UPDATE cart SET quantity = quantity - 1 WHERE id_product = ?";
         $stmtUpdate = $conn->prepare($updateSql);
         $stmtUpdate->bind_param("i", $productId);
         if ($stmtUpdate->execute()) {
-            echo 'success - quantity updated';
+            echo 'success';
         } else {
-            echo 'error - quantity update';
+            echo 'error';
         }
+        $stmtUpdate->close();
     } else {
-        // Удаление из корзины
         $deleteSql = "DELETE FROM cart WHERE id_product = ?";
         $stmtDelete = $conn->prepare($deleteSql);
         $stmtDelete->bind_param("i", $productId);
         if ($stmtDelete->execute()) {
-            echo 'success - product deleted';
+            echo 'success';
         } else {
-            echo 'error - product deletion';
+            echo 'error';
         }
-    }
-
-    $stmtGetQuantity->close();
-    if (isset($stmtUpdate)) {
-        $stmtUpdate->close();
-    }
-    if (isset($stmtDelete)) {
         $stmtDelete->close();
     }
+
     $conn->close();
 }
 ?>
