88372b681ef9b9bfedef59aac6839a4ce5d9d026
Leya <leya.min@mail.ru>
2023-12-08 19:27:35 +0300

    make order w empty cart

--- makeOrder.php
+++ makeOrder.php
@@ -16,6 +16,27 @@ $first_name = $_POST['first_name'];
 $number = $_POST['number'];
 $address_id = $_POST['address'];
 
+// Проверка наличия товаров в корзине
+$checkCartSql = "SELECT COUNT(*) as cartCount FROM cart";
+$resultCartCount = $conn->query($checkCartSql);
+
+if ($resultCartCount->num_rows > 0) {
+    $cartCountData = $resultCartCount->fetch_assoc();
+    $cartCount = $cartCountData['cartCount'];
+
+    // Если в корзине есть товары, выполнить оформление заказа
+    if ($cartCount > 0) {
+        // Ваш существующий код для оформления заказа
+    } else {
+        echo "Нет товаров в корзине. Оформление заказа невозможно.";
+    }
+} else {
+    echo "Ошибка при проверке корзины: " . $conn->error;
+}
+
+$conn->close();
+
+
 // Проверка номера телефона
 if (strlen($number) !== 12 || substr($number, 0, 3) !== '+79') {
     echo 'Некорректный номер телефона';
@@ -37,7 +58,8 @@ $summarySql = "SELECT SUM(cart.quantity) AS totalQuantity, SUM(product.product_p
                 JOIN product ON cart.id_product = product.id_product";
 $resultSummary = $conn->query($summarySql);
 
-if ($resultSummary->num_rows > 0) {
+if ($resultSummary->num_rows > 0)
+{
     $summary = $resultSummary->fetch_assoc();
     $totalQuantity = $summary['totalQuantity'];
     $totalPrice = $summary['totalPrice'];
@@ -45,47 +67,52 @@ if ($resultSummary->num_rows > 0) {
     // Вставка данных в таблицу orders
     $insertOrderSql = "INSERT INTO orders (datetime, count_of_products, id_client, id_address, total) 
                     VALUES (NOW(), '$totalQuantity', '$client_id', '$address_id', '$totalPrice')";
-    if ($conn->query($insertOrderSql) === TRUE) {
+    if ($conn->query($insertOrderSql) === TRUE)
+    {
         $order_id = $conn->insert_id; // Получение ID нового заказа
-    } else {
+    }
+    else
+    {
         echo "Ошибка вставки данных заказа: " . $conn->error;
         exit();
     }
 
 
-        // Получение данных о товарах в корзине
+    // Получение данных о товарах в корзине
     $cartItemsSql = "SELECT cart.*, product.product_price
-        FROM cart
-        JOIN product ON cart.id_product = product.id_product";
+    FROM cart
+    JOIN product ON cart.id_product = product.id_product";
     $resultCartItems = $conn->query($cartItemsSql);
 
-    if ($resultCartItems->num_rows > 0) {
-    while ($row = $resultCartItems->fetch_assoc()) {
-    $productId = $row['id_product'];
-    $sizeId = $row['id_size'];
-    $count = $row['quantity'];
-    $productPrice = $row['product_price'];
-    $sum = $count * $productPrice;
+    if ($resultCartItems->num_rows > 0)
+    {
+        while ($row = $resultCartItems->fetch_assoc())
+        {
+            $productId = $row['id_product'];
+            $sizeId = $row['id_size'];
+            $count = $row['quantity'];
+            $productPrice = $row['product_price'];
+            $sum = $count * $productPrice;
 
-    // Вставка данных в таблицу order_details
-    $insertOrderDetailsSql = "INSERT INTO order_details (id_product, id_order, id_size, count, sum) 
-                            VALUES ('$productId', '$order_id', '$sizeId', '$count', '$sum')";
-    if ($conn->query($insertOrderDetailsSql) !== TRUE) {
-        echo "Ошибка вставки данных деталей заказа: " . $conn->error;
-        exit(); }
-
-       /*  $updateAvailableSizesSql = "UPDATE available_sizes SET count = count - '$count' WHERE id_product = '$productId' AND id_sizes = '$sizeId'";
-        if ($conn->query($updateAvailableSizesSql) !== TRUE) {
-            echo "Ошибка обновления данных в таблице available_sizes: " . $conn->error;
-            exit();} */
+            // Вставка данных в таблицу order_details
+            $insertOrderDetailsSql = "INSERT INTO order_details (id_product, id_order, id_size, count, sum) 
+                                    VALUES ('$productId', '$order_id', '$sizeId', '$count', '$sum')";
+            if ($conn->query($insertOrderDetailsSql) !== TRUE)
+            {
+                echo "Ошибка вставки данных деталей заказа: " . $conn->error;
+                exit();
             }
-        } else {
-            echo "Нет товаров в корзине"; }
-    } else {
-        echo "Нет данных о товарах";
+        }
     }
-
-    
+    else
+    {
+        echo "Нет товаров в корзине";
+    }
+}
+else
+{
+    echo "Нет данных о товарах";
+}
 
     $conn->close();
 
